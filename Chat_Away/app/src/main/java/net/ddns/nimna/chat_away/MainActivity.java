040e8384b74b868e9a9a7372e56785e23b157f42
Nimna Ekanayaka <nekanayaka@hollandcollege.com>
2016-01-25 04:20:11 -0400

    Get the logo to display at the beginning, but it is pixelated.

--- Chat_Away/app/src/main/java/net/ddns/nimna/chat_away/MainActivity.java
+++ Chat_Away/app/src/main/java/net/ddns/nimna/chat_away/MainActivity.java
@@ -3,7 +3,11 @@ package net.ddns.nimna.chat_away;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Nimna on 24/01/2016.
@@ -13,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            Thread.sleep(5000);
-            Intent intent = new Intent(MainActivity.this, SigninActivity.class);
-            startActivity(intent);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+//        try {
+//            Thread.sleep(5000);
+//            Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+//            startActivity(intent);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
