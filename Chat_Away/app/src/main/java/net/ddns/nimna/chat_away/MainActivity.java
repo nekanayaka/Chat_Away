cf7d866bc0fa27fa6f018c6980022358ad1ccaef
unknown <nekanayaka@CCCIS0228-17.hc.local>
2016-02-03 15:49:02 -0400

    Updated the launcher icon.

--- Chat_Away/app/src/main/java/net/ddns/nimna/chat_away/MainActivity.java
+++ Chat_Away/app/src/main/java/net/ddns/nimna/chat_away/MainActivity.java
@@ -26,10 +26,11 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 5000);
-        DBHelper db = new DBHelper(this);
-        SQLiteDatabase mydatabase = openOrCreateDatabase("messages.db",MODE_PRIVATE,null);
-        db.onCreate(mydatabase);
-        db.saveMessage(0, 1, "message test", "date11");
+        //just commenting out for now to avoid the crash.
+//        DBHelper db = new DBHelper(this);
+//        SQLiteDatabase mydatabase = openOrCreateDatabase("messages.db",MODE_PRIVATE,null);
+//        db.onCreate(mydatabase);
+//        db.saveMessage(0, 1, "message test", "date11");
 //        try {
 //            Thread.sleep(5000);
 //            Intent intent = new Intent(MainActivity.this, SigninActivity.class);
