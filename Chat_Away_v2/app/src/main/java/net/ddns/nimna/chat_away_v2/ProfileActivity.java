daed426730ba8fd89422bee941d9b71d1c3e0dfb
unknown <mhutchinson110082@CCCIS0228-16.hc.local>
2016-02-10 15:59:03 -0400

    Fiddling with location services

--- Chat_Away_v2/app/src/main/java/net/ddns/nimna/chat_away_v2/ProfileActivity.java
+++ Chat_Away_v2/app/src/main/java/net/ddns/nimna/chat_away_v2/ProfileActivity.java
@@ -1,17 +1,26 @@
 package net.ddns.nimna.chat_away_v2;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.location.*;
+
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by Nimna Ekanayaka on 20/01/2016.
@@ -45,9 +54,22 @@ public class ProfileActivity extends AppCompatActivity {
         tvEmailProfile.setText(email);
         tvAccountLevelProfile.setText(accountLevel);
         btnChat = (Button)findViewById(R.id.btnChat);
+
         btnChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+//                LocationManager local = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//                Location location = null;
+//                Criteria criteria = new Criteria();
+//                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+//
+//                    location = local.getLastKnownLocation(local.getBestProvider(criteria, true));
+//
+//                location.getLatitude();
+//                location.getLongitude();
+//                Log.d("LOCATION", "Lat="+location.getLatitude()+" Long="+location.getLongitude());
+
                 Intent intent = new Intent(ProfileActivity.this, MessagingActivity.class);
                 intent.putExtra("username", username);
                 intent.putExtra("RECIPIENT_ID", "1");
